88d49f6ce9f07990f8c53b7bb3bee564abe222c6
Sébastien Thuret <contact@sebastien-thuret.fr>
2021-02-12 13:22:02 +0100

    Create list page

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Trick;
 use App\Entity\TrickGroup;
+use App\Repository\TrickRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,6 +23,21 @@ class TrickController extends AbstractController
     {
         return $this->render('tricks/show.html.twig', [
             'trick'      => $trick,
+            'trickGroup' => $trickGroup
+        ]);
+    }
+
+    /**
+     * @param TrickRepository $trickRepository
+     * @param TrickGroup|null $trickGroup
+     * @return Response
+     */
+    public function list(?TrickGroup $trickGroup, TrickRepository $trickRepository): Response
+    {
+        $tricks = $trickRepository->findList($trickGroup);
+
+        return $this->render('tricks/list.html.twig', [
+            'tricks'     => $tricks,
             'trickGroup' => $trickGroup
         ]);
     }
