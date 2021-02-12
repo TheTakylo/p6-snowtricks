a8b81c76f0906a43073ae0a47a272d1918f40cbb
Sébastien Thuret <contact@sebastien-thuret.fr>
2021-02-12 14:36:44 +0100

    Login & Register form

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserRegistrationType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    public function register(Request $request): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('pages_index');
+        }
+
+        $user = new User();
+        $form = $this->createForm(UserRegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // TODO
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('pages_index');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
