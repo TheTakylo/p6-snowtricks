ff49b1a6796ab233fe04644761f9f77a7b0aa398
Sébastien Thuret <contact@sebastien-thuret.fr>
2021-02-18 11:51:58 +0100

    Use annotations for routes instead of yaml

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,9 +5,16 @@ namespace App\Controller;
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
+
+    /**
+     * @Route("/", name="pages_index")
+     * @param TrickRepository $trickRepository
+     * @return Response
+     */
     public function index(TrickRepository $trickRepository): Response
     {
         $tricks = $trickRepository->findForHomePage();
