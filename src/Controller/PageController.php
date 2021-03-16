b4e677ab93c643e2e3789694d72c029049e0e48b
Sébastien Thuret <contact@sebastien-thuret.fr>
2021-03-16 12:40:14 +0100

    Remove unused import

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\TrickRepository;
-use App\Service\VideoServiceFinder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
